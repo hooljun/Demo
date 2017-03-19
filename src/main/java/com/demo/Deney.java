a009be5f10ea32be78d4a4c5e0cc2bf688395c7e
hooljun <hooljun@163.com>
2017-03-19 11:39:32 +0800

    Update Deney.java

--- src/main/java/com/demo/Deney.java
+++ src/main/java/com/demo/Deney.java
@@ -4,5 +4,14 @@ public class Deney {
 
 	public static void main(String[] args) {
 		System.out.println("deney");
+		System.out.println("deney");
+		System.out.println("deney");
+		System.out.println("deney");
+		System.out.println("deney");
+		
+		System.out.println("deney");
+		System.out.println("deney");
+		System.out.println("deney");
+		
 	}
 }
